65736e8082c3dfc4882b287ec08b28f7fef0762f
i5ting <i5ting@126.com>
2021-12-14 14:56:21 +0800

    refact, please ignore

--- example/src/layers/6/header.tsx
+++ example/src/layers/6/header.tsx
@@ -1,16 +1,16 @@
 import React from 'react';
-import { Stateview, Layer, useStateContext } from '~/index';
+import { Stateview, Layer } from '~/index';
 import { Play0, Play1, Like0, Like1 } from './headers/index';
 
 export function Header(props: any) {
   return <>
     <Stateview block default='play1' height="200px" >
-      <Layer router='play0' component={<Play0 name='跳转到未登录状态' />} />
-      <Layer router='play1' component={<Play1 name='跳转到可领状态' />} />
+      <Layer router='play0' component={<Play0/>} />
+      <Layer router='play1' component={<Play1/>} />
     </Stateview>
     <Stateview block default='like0' height="200px" >
-      <Layer router='like0' component={<Like0 name='跳转到未登录状态' />} />
-      <Layer router='like1' component={<Like1 name='跳转到可领状态' />} />
+      <Layer router='like0' component={<Like0/>} />
+      <Layer router='like1' component={<Like1/>} />
     </Stateview>
   </>;
 }
--- example/src/layers/6/panel.tsx
+++ example/src/layers/6/panel.tsx
@@ -4,12 +4,6 @@ import { P1,P2 } from './panels/index';
 import styles from './tab.less';
 
 export function Panel(props: any) {
-  const { stateview } = useStateContext()
-
-  function sayHello() {
-    stateview.show('unlogin')
-  }
-
   return <>
     <Stateview default='p1' height="200px">
       <Layer router='p1' component={<P1/>} />
