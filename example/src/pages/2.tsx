ecf853d4d4670599c99d71ea1e206f30be198125
i5ting <i5ting@126.com>
2021-12-16 11:44:50 +0800

    document, please ignore

--- example/src/pages/2.tsx
+++ example/src/pages/2.tsx
@@ -2,13 +2,14 @@ import React from 'react';
 
 import { Stateview, Layer } from '~/index';
 import { Logined, UnLogin } from '../layers/1';
+import styles from './index.less';
 
 /**
  * 嵌套Demo：3个状态切换 
  */ 
 export default () => {
   return (
-    <Stateview default='unlogin' height="200px">
+    <Stateview default='unlogin' className={styles.panel}>
       <Layer router='logined' component={<Logined name='跳转到未登录状态' />} />
       <Layer router='unlogin' component={<UnLogin name='跳转到登录状态' />} />
     </Stateview>
