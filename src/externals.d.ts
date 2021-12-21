43a5341984de846920fdd7d993e97ab706a02afd
i5ting <i5ting@126.com>
2021-12-21 20:52:46 +0800

    document, please ignore

--- src/externals.d.ts
+++ src/externals.d.ts
@@ -2,3 +2,10 @@ declare module '*.less' {
     const resource: {[key: string]: string};
     export = resource;
 }
+
+declare interface LayerProps {
+    state: string;
+    component?: any;
+    children?: any;
+    data?: any;
+}
--- src/layer.tsx
+++ src/layer.tsx
@@ -1,12 +1,5 @@
 import React, { Fragment } from 'react';
 
-interface LayerProps {
-    state: string;
-    component?: any;
-    children?: any;
-    data?: any;
-}
-
 export function Layer(props: LayerProps) {
     return <Fragment>{props.component}</Fragment>;
 }
